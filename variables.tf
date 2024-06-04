fd9cdffe7dd4852f39d4f5e528730e730be20871
beluvigishreegagana <beluvigishreegagana@gmail.com>
2024-06-04 16:14:24 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "resource_group_name" {
+    type = string
+    description = "resource group name"
+  
+}
+variable "resource_group_location" {
+    type = string
+    description = "resource group location"
+  
+}
