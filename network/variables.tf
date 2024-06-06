9d2deec50555f84259eeefbfbaf63ea90173faba
beluvigishreegagana <beluvigishreegagana@gmail.com>
2024-06-06 11:47:08 +0530

    Create variables.tf

---
+++ network/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "resource_group_location" {
+  type = string
+}
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "vnet_address_space" {
+  type = list(string)
+}
